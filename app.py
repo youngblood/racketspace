d6bc3090d468cf90583430c7cda37b11f13d2d71
Reed Youngblood <reedayoungblood@gmail.com>
2020-09-19 17:25:43 -0500

    adds crude model search and NN table

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 import math
 import numpy as np
 import pandas as pd
@@ -6,6 +7,8 @@ from sklearn.decomposition import PCA
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+# import dash_bootstrap_components as dbc
+import dash_table
 from dash.dependencies import Input, Output
 import plotly.express as px
 
@@ -14,7 +17,7 @@ import plotly.express as px
 
 app = dash.Dash(__name__)#, external_stylesheets=external_stylesheets)
 server = app.server
-app.title = "RacketSpace"
+app.title = "RktSpc"
 
 # df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')	
 raw_df = pd.read_csv('data/racket_specs.csv')
@@ -28,15 +31,19 @@ df = raw_df[(raw_df['Head Size (in)']>=60) & \
 			(raw_df['Beam Width (avg. mm)']<=50) & \
 			(raw_df['Balance (pts)'] >= -20) & \
             (raw_df['Balance (pts)'] <= 20) & \
-            (raw_df['String Density (intersections / sq. in.)'] >= 1) & \
-            (raw_df['String Density (intersections / sq. in.)'] <= 6) & \
+            (raw_df['String Density (X / sq. in.)'] >= 1) & \
+            (raw_df['String Density (X / sq. in.)'] <= 6) & \
             (raw_df['Swingweight'] >= 100) & \
             (raw_df['Swingweight'] <= 600) & \
             (raw_df['Stiffness'] >= 10) & \
             (raw_df['Stiffness'] <= 100)].copy()
 
 df.rename(columns={'url': 'tw_url', 'img_url': 'tw_img_url'},
+				   # 'Unnamed: 0': 'ID', 'mfr': 'Manufacturer'},
 		  inplace=True)
+
+df['ID'] = df.index
+df['Distance'] = 0
 
 head_size_jitter 	  = 0.5
 strung_weight_jitter  = 0.05
@@ -53,14 +60,19 @@ df['Head Size (in) jittered'] = df['Head Size (in)'].apply(add_jitter, max_jitte
 df['Strung Weight (oz) jittered'] = df['Strung Weight (oz)'].apply(add_jitter, max_jitter=strung_weight_jitter)
 df['Beam Width (avg. mm) jittered'] = df['Beam Width (avg. mm)'].apply(add_jitter, max_jitter=beam_width_jitter)
 df['Balance (pts) jittered'] = df['Balance (pts)'].apply(add_jitter, max_jitter=balance_jitter)
-df['String Density (intersections / sq. in.) jittered'] = df['String Density (intersections / sq. in.)'].apply(add_jitter, max_jitter=string_density_jitter)
+df['String Density (X / sq. in.) jittered'] = df['String Density (X / sq. in.)'].apply(add_jitter, max_jitter=string_density_jitter)
 df['Swingweight jittered'] = df['Swingweight'].apply(add_jitter, max_jitter=swingweight_jitter)
 df['Stiffness jittered'] = df['Stiffness'].apply(add_jitter, max_jitter=stiffness_jitter)
 
 # Standardization of Features and PCA
-features_to_norm = ['Head Size (in)', 'Strung Weight (oz)', 'Beam Width (avg. mm)',
-                   'Balance (pts)', 'String Density (intersections / sq. in.)', 
-                   'Swingweight', 'Stiffness']
+features_to_norm = ['Head Size (in)',
+					'Strung Weight (oz)',
+					'Balance (pts)',
+					# 'Stiffness',
+					# 'Beam Width (avg. mm)',
+					# 'String Density (X / sq. in.)',
+                    # 'Swingweight',
+					]
 
 types_of_norming = {} # min-max-scaler, 
 
@@ -97,7 +109,7 @@ def replace_blank_urls_with_ebay(s):
     return s[0] if type(s[0]) == str and s[0] != '' else \
         'https://www.ebay.com/sch/i.html?_nkw={}'.format(s[1].replace(' ','+'))
 
-df['url'] = df[['tw_url','name']].apply(replace_blank_urls_with_ebay, axis=1)
+df['url'] = df[['tw_url','Model']].apply(replace_blank_urls_with_ebay, axis=1)
 
 df['Current/Old Models'] = df['tw_url'].apply(lambda x: \
 	'Current Model' if type(x) == str and x != '' else 'Old Model')
@@ -123,10 +135,10 @@ app.layout = html.Div(style={'padding':"10px"}, children=[
 					dcc.Dropdown(
 						className='twelve columns',
 						id='mfr-dropdown',
-						options=[{'label': mfr, 'value': mfr} for mfr in sorted(list(df['mfr'].unique()))],
+						options=[{'label': mfr, 'value': mfr} for mfr in sorted(list(df['Manufacturer'].unique()))],
 						multi=True,
 						style={'background-color':colors['white'], 'color':colors['black'], 'border-radius':'4px'},
-						value=sorted(list(df['mfr'].unique()))
+						value=sorted(list(df['Manufacturer'].unique()))
 					),
 				]),
 				html.Div(className='twelve columns', children=[
@@ -284,7 +296,7 @@ app.layout = html.Div(style={'padding':"10px"}, children=[
 							 {'label': 'Strung Weight (oz)', 'value': 'Strung Weight (oz)'},
 							 {'label': 'Beam Width (avg. mm)', 'value': 'Beam Width (avg. mm)'},
 							 {'label': 'Balance (pts)', 'value': 'Balance (pts)'},
-							 {'label': 'String Density (intersections / sq. in.)', 'value': 'String Density (intersections / sq. in.)'},
+							 {'label': 'String Density (intersections / sq. in.)', 'value': 'String Density (X / sq. in.)'},
 							 {'label': 'Swingweight', 'value': 'Swingweight'},
 							 {'label': 'Stiffness', 'value': 'Stiffness'},
 							 {'label': 'Current/Old Models', 'value': 'Current/Old Models'}],
@@ -299,7 +311,7 @@ app.layout = html.Div(style={'padding':"10px"}, children=[
 							 {'label': 'Strung Weight (oz)', 'value': 'Strung Weight (oz)'},
 							 {'label': 'Beam Width (avg. mm)', 'value': 'Beam Width (avg. mm)'},
 							 {'label': 'Balance (pts)', 'value': 'Balance (pts)'},
-							 {'label': 'String Density (intersections / sq. in.)', 'value': 'String Density (intersections / sq. in.)'},
+							 {'label': 'String Density (intersections / sq. in.)', 'value': 'String Density (X / sq. in.)'},
 							 {'label': 'Swingweight', 'value': 'Swingweight'},
 							 {'label': 'Stiffness', 'value': 'Stiffness'},
 							 {'label': 'Current/Old Models', 'value': 'Current/Old Models'}],
@@ -335,27 +347,65 @@ app.layout = html.Div(style={'padding':"10px"}, children=[
 					 {'label': 'Strung Weight (oz)', 'value': 'Strung Weight (oz)'},
 					 {'label': 'Beam Width (avg. mm)', 'value': 'Beam Width (avg. mm)'},
 					 {'label': 'Balance (pts)', 'value': 'Balance (pts)'},
-					 {'label': 'String Density (intersections / sq. in.)', 'value': 'String Density (intersections / sq. in.)'},
+					 {'label': 'String Density (intersections / sq. in.)', 'value': 'String Density (X / sq. in.)'},
 					 {'label': 'Swingweight', 'value': 'Swingweight'},
 					 {'label': 'Stiffness', 'value': 'Stiffness'},
 					 {'label': 'Current/Old Models', 'value': 'Current/Old Models'},
 					 {'label': 'Principal Component 3', 'value': 'Principal Component 3'}],
-			value='Beam Width (avg. mm)'
+			value='Balance (pts)'
 		),
 	]),
 	html.Div(className='nine columns', id='graph-div', style={'outline': '1px solid #794bc4','margin-top':'20px'}, children=[
 		dcc.Graph(
 	        id='racquetspace_graph',
 	    ),
-	]),  
+	]),
+	html.Div([
+		# dcc.Markdown('''**Click a racket in the graph above or search for one below to see details and most similar rackets.**'''),
+		html.Label('Click a racket in the graph above or search for one below to see details and most similar rackets.', style={'margin-top':'10px'}),
+		dcc.Dropdown(
+			id='model-dropdown',
+			options=[{'label': model, 'value': model} for model in sorted(list(df['Model'].unique()))],
+			multi=False,
+			style={'background-color':colors['white'], 'color':colors['black'], 'border-radius':'4px'},
+			value=None
+		),
+		dash_table.DataTable(
+			id='selected-rackets-table',
+			columns=[
+				{"name": i, "id": i}
+				for i in ["ID", # TODO: hide this one
+						  "Distance",
+						  "Manufacturer",
+						  "Model",
+						  "Head Size (in)",
+						  "Strung Weight (oz)",
+						  "Balance (pts)",
+						  "Stiffness",
+						  "Beam Width (avg. mm)",
+						  "Swingweight",
+						  "String Density (X / sq. in.)",
+						  "url"]
+			],
+			# data=
+		)
+	], className='ten columns'),
+
+	html.Div([
+		dcc.Markdown("""
+	                **Click Data**
+
+	                Click on points in the graph.
+	            """),
+		html.Pre(id='click-data'),
+	], className='nine columns'),
 ])
 
 @app.callback(
 	Output('mfr-dropdown', 'value'),
 	[Input('select-all-btn', 'n_clicks')])
 def select_all_mfrs(select_all):
-	print(select_all)
-	return sorted(list(df['mfr'].unique()))
+	return sorted(list(df['Manufacturer'].unique()))
 
 @app.callback(
     Output('racquetspace_graph', 'figure'),
@@ -373,7 +423,7 @@ def select_all_mfrs(select_all):
      Input('x-axis-dropdown', 'value'),
      Input('y-axis-dropdown', 'value'),
      Input('color-dropdown', 'value')])
-def update_figure(mfrs, head_size_range, strung_weight_range,
+def update_scatter(mfrs, head_size_range, strung_weight_range,
 				  balance_range, beam_width_range, string_density_range, 
 				  swingweight_range, stiffness_range, jitter, 
 				  current_models_only, tabs, x_axis, y_axis, color):
@@ -381,7 +431,7 @@ def update_figure(mfrs, head_size_range, strung_weight_range,
     current_model_states = ['Current Model'] if current_models_only else \
     					   ['Current Model', 'Old Model']
 
-    filtered_df = df[(df['mfr'].isin(mfrs)) & \
+    filtered_df = df[(df['Manufacturer'].isin(mfrs)) & \
     				 (df['Current/Old Models'].isin(current_model_states)) & \
     				 (df['Head Size (in)'] >= head_size_range[0]) & \
     				 (df['Head Size (in)'] <= head_size_range[1]) & \
@@ -391,8 +441,8 @@ def update_figure(mfrs, head_size_range, strung_weight_range,
     				 (df['Balance (pts)'] <= balance_range[1]) & \
     				 (df['Beam Width (avg. mm)'] >= beam_width_range[0]) & \
     				 (df['Beam Width (avg. mm)'] <= beam_width_range[1]) & \
-    				 (df['String Density (intersections / sq. in.)'] >= string_density_range[0]) & \
-    				 (df['String Density (intersections / sq. in.)'] <= string_density_range[1]) & \
+    				 (df['String Density (X / sq. in.)'] >= string_density_range[0]) & \
+    				 (df['String Density (X / sq. in.)'] <= string_density_range[1]) & \
     				 (df['Swingweight'] >= swingweight_range[0]) & \
     				 (df['Swingweight'] <= swingweight_range[1]) & \
     				 (df['Stiffness'] >= stiffness_range[0]) & \
@@ -417,14 +467,15 @@ def update_figure(mfrs, head_size_range, strung_weight_range,
     # Calculate dynamic PCA columns if ***checkbox enabled***
     fig = px.scatter(filtered_df, 
 				 x=x_col, y=y_col, 
-                 color=color, hover_name='name',
+                 color=color, hover_name='Model',
                  labels={
                  	x_col: x_col,
                  	y_col: y_col,
                  	color: color.split(' (')[0]
                  	#color: color.replace(' (','\n\n\n\n(#')
                  },
-                 hover_data=['Head Size (in)',
+                 hover_data=['ID',
+							 'Head Size (in)',
                  			 'Strung Weight (oz)',
                  			 'Balance (pts)', 
                  			 'Beam Width (avg. mm)',
@@ -441,6 +492,45 @@ def update_figure(mfrs, head_size_range, strung_weight_range,
     fig.update_layout(transition_duration=1000)
 
     return fig
+
+@app.callback(
+	Output('model-dropdown', 'value'),
+	[Input('racquetspace_graph', 'clickData')])
+	 # Input('tabs', 'value'),
+	 # Input('x-axis-dropdown', 'value'),
+	 # Input('y-axis-dropdown', 'value'),
+	 # Input('color-dropdown', 'value')])
+def click_overwrite_search(clickData):
+	try:
+		return clickData['points'][0]['hovertext']
+	except TypeError:
+		return None
+
+def calculate_distance(inputs, targets):
+    return np.sqrt(np.sum([(a-b)**2 for a, b in zip(inputs, targets)]))
+
+@app.callback(
+	Output('selected-rackets-table', 'data'),
+	[Input('model-dropdown', 'value')])
+def select_racket_table(model_dropdown):
+	try:
+		id = int(df[df['Model'] == model_dropdown]['ID'])
+		# df['Distance'] = id
+		x = float(df[df['ID'] == id]['Principal Component 1'])
+		y = float(df[df['ID'] == id]['Principal Component 2'])
+		z = float(df[df['ID'] == id]['Principal Component 3'])
+		df['Distance'] = df[['Principal Component 1', 'Principal Component 2', 'Principal Component 3']].apply(
+			calculate_distance, targets=[x, y, z], axis=1)
+		return df.sort_values("Distance").head(10).to_dict('records')
+	except TypeError:
+		return None
+
+@app.callback(
+	Output('click-data', 'children'),
+	[Input('racquetspace_graph', 'clickData')])
+def select_racket_json(clickData):
+	return json.dumps(clickData, indent=2)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
